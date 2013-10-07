05c29e588dff93a3a84114cefccce4c8d28bc268
ferron <ferronrsmith@gmail.com>
2013-10-07 12:23:17 -0400

    added minified version

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,9 @@ module.exports = function (grunt) {
             },
             unstable: {
                 configFile: 'config/karma.unstable.conf.js'
+            },
+            minify: {
+                configFile: 'config/karma.min.conf.js'
             }
         },
         watch: {
@@ -38,14 +41,23 @@ module.exports = function (grunt) {
                     message: 'Finish Linting & Running Tests' //required
                 }
             }
+        },
+        uglify: {
+            datespy: {
+                files: {
+                    'dest/datespy.min.js': ['js/src/datespy.js']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-karma');
     grunt.registerTask('test', ['jslint', 'karma:stable']);
-    grunt.registerTask('default', ['jslint', 'karma:stable']);
+    grunt.registerTask('default', ['jslint', 'karma:stable', 'uglify', 'karma:minify']);
+    grunt.registerTask('release', ['uglify', 'karma:minify']);
 
 };
---
+++ config/karma.min.conf.js
@@ -0,0 +1,21 @@
+/*global module */
+module.exports = function (config) {
+    "use strict";
+    config.set({
+        basePath: '../',
+        frameworks: ['jasmine'],
+        files: [
+            'js/lib/angular/angular-stable-latest.js',
+            'js/lib/angular/angular-stable-mocks.js',
+            'dest/*.js',
+            'test/src/*.js',
+            'test/spec/*.js'
+        ],
+        autoWatch: true,
+        browsers: ['Chrome'],
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+    });
+};
